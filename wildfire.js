bc1b4e10a7ef6a8b839d0459733f6e93bc97e982
Sarmishta Velury <sarmishta.velury@gmail.com>
2019-12-01 15:22:15 -0500

    Legend trial

--- wildfire.js
+++ wildfire.js
@@ -96,8 +96,35 @@ function plotChartByYear(hexGroup, dataByYear) {
 
     //TODO: Add color attribute or add colorscale
 	// This radius can be changed with a d3.scaleXXX
+
+	//Append a defs (for definition) element to your SVG
+	var defs = svg.append("defs");
+
+	//Append a linearGradient element to the defs and give it a unique id
+	var linearGradient = defs.append("linearGradient")
+		.attr("id", "linear-gradient");
 	
-	console.log(max_fires)
+	linearGradient
+	.attr("x1", "0%")
+	.attr("y1", "0%")
+	.attr("x2", "100%")
+	.attr("y2", "0%");
+
+	//Set the color for the start (0%)
+	linearGradient.append("stop")
+	.attr("offset", "0%")
+	.attr("stop-color", d3.interpolateRdYlBu(1)); //light blue
+
+	//Set the color for the end (100%)
+	linearGradient.append("stop")
+	.attr("offset", "100%")
+	.attr("stop-color", d3.interpolateRdYlBu(0)); //dark blue
+
+	svg.append("rect")
+	.attr("transform", "translate(1000, 200)")
+    .attr("width", 300)
+    .attr("height", 20)
+    .style("fill", "url(#linear-gradient)");
 }
 
 
